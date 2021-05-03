b8e22fd25528762eb5ad0949d932285de6034686
Artur Majewski <artur.majewski@gecko-lab.com>
2021-05-03 23:37:23 +0200

    Add graphics module, clean up the note-taking app ;D

--- szatan.py
+++ szatan.py
@@ -1,15 +1,34 @@
 # !/usr/bin/python3
 
+#from graphics import *
+#win = GraphWin()
+
+
+def create_or_append(line):
+    file_descriptor = open('data.txt','a', encoding='utf-8')
+    file_descriptor.write(line+"\n")
+    
+def read_from_file():
+    file_descriptor = open('data.txt','r', encoding='utf-8')
+    for val in file_descriptor.readlines():
+        print(val.strip())
 res=""
 loglist = []
+
 while (res!="q"):
-    res=input("add note => [ENTER], [q] to quit, [s] to show all notes so far:\n")
-    if(res!="s"):
-        loglist.append(res)
-    else: # s
+    res=input("add note => [ENTER], [q]quit, [s]show all notes so far, [r]ead saved data:\n")
+    if(res=="s"):
+        
         print("Notes so far:")
         for it in loglist:
-            print(it)
+            print(it.strip())
+    elif(res=="r"):
+        read_from_file()
+    else:
+        if(res!='q'):
+            loglist.append(res+"\n")
+            create_or_append(res)
+
 #val = 665
 
 #print("Szatan: " + str(val))
