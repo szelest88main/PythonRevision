fd3bd9b3856d947d0f398ac5a9872e20d3e6fe12
Artur Majewski <artur.majewski@gecko-lab.com>
2021-06-12 21:29:18 +0200

    Remove obsolete code and fix the main file name

--- notes.py
+++ notes.py
@@ -40,32 +40,3 @@ def main():
     notes()
 
 main()
-
-#val = 665
-
-#print("Szatan: " + str(val))
-
-#ok, let's do fizzbuzz
-
-# for iterator in range (1,101):
-#     if(iterator%3==0 and iterator%5==0):
-#         print("FizzBuzz")
-#     elif (iterator%3==0):
-#         print("Fizz")
-#     elif (iterator%5==0):
-#         print("Buzz")
-#     else:
-#         print(str(iterator))
-
-
-## loop
-# for it in range(1,101):
-#     print("Param " + str(it))
-
-## if
-# if (val==666):
-#     print("Blaaa")
-# elif (val==665):
-#     print("Bluuu")
-# else:
-#     print("Bleee")
