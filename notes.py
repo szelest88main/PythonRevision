4a6b0d71fd0d824251ffd74a8c87454bdc9b4907
Artur Majewski <artur.majewski@gecko-lab.com>
2021-06-12 22:19:26 +0200

    Clean up

--- notes.py
+++ notes.py
@@ -2,30 +2,32 @@
 
 from datetime import datetime
 
+# append the line to the 'data.txt' file or create the file first it it doesn't exist
 def create_or_append(line):
     file_descriptor = open('data.txt','a', encoding='utf-8')
     file_descriptor.write(line+"\n")
     file_descriptor.close()
     
+logs_list = []
+
+# read all the lines from 'data.txt', print them to screen and add them to logs_list list.
 def read_from_file():
-    global loglist
-    loglist = []
+    global logs_list # "global": don't override the global logs_list: use it 
+    logs_list = []
     file_descriptor = open('data.txt','r', encoding='utf-8')
     for val in file_descriptor.readlines():
-        loglist.append(val)
+        logs_list.append(val)
         print(val.strip())
     file_descriptor.close()
 
-res=""
-loglist = []
 
 def notes():
-    global res
+    res=""
     while (res!="q"):
         res=input("add note => [ENTER], [q]uit, [s]how all notes so far, [r]ead saved data:\n")
         if(res=="s"):
             print("Notes so far:")
-            for it in loglist:
+            for it in logs_list:
                 print(it.strip())
         elif(res=="r"):
             read_from_file()
@@ -33,7 +35,7 @@ def notes():
             now = datetime.now()
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             string_with_date = dt_string+"\t"+res 
-            loglist.append(string_with_date+"\n")
+            logs_list.append(string_with_date+"\n")
             create_or_append(string_with_date)
 
 def main():
